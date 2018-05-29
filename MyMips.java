a4de07acd7f577b8e9066632de176d7ff340e6d1
Giuseppe Fiorentino Neto <netogiuseppe@hotmail.com>
2018-05-29 11:24:35 -0300

    Update MyMips.java

--- MyMips.java
+++ MyMips.java
@@ -4,6 +4,7 @@ package br.ufrpe.deinfo.aoc.mips.example;
 import java.io.IOException;
 
 import jline.console.ConsoleReader;
+import br.ufrpe.deinfo.aoc.mips.InvalidMemoryAlignmentExpcetion;
 import br.ufrpe.deinfo.aoc.mips.MIPS;
 import br.ufrpe.deinfo.aoc.mips.Simulator;
 import br.ufrpe.deinfo.aoc.mips.State;
@@ -12,13 +13,19 @@ public class MyMIPS implements MIPS{
 	
 	private boolean pcPlus4;
 
+	@SuppressWarnings("unused")
+	private ConsoleReader console;
 	
+	public MyMIPS() throws IOException {
+		this.console = Simulator.getConsole();
+	}
 	@Override
 	public void execute(State s) throws Exception {
 		if(s.getPC().equals(0)){
 			s.writeRegister(28,  0x1800); //Global Pointer Localization  
 			s.writeRegister(29,  0x3ffc); //Stack Pointer Localization
 		}
+		
 		
 		String inst = completToLeft(Integer.toBinaryString(s.readInstructionMemory(s.getPC())), '0', 32);
 		String op = inst.substring(0,6);
@@ -34,7 +41,7 @@ public class MyMIPS implements MIPS{
 		pcPlus4 = true;
 	}
 
-	private static String completToLeft(String str, char c, int i) { //TODO remover static
+	private String completToLeft(String str, char c, int i) { 
 		// Caso a instrução tenha varios zeros antes este sera ignorado, mas é preciso usa-lo. Então essa classe existe
 		// Pensar em usar o >> é deslocamento logico, preenche sempre com zero a esquerda
 		String nstr = Character.toString(c);
@@ -48,8 +55,27 @@ public class MyMIPS implements MIPS{
 		return null;
 	}
 
-	private void tipoI(State s, String inst, String op) {
-		// TODO Auto-generated method stub
+	private void tipoI(State s, String inst, String op) throws InvalidMemoryAlignmentExpcetion {
+		Integer rs = Integer.parseInt(inst.substring(6,11),2);
+		Integer rt = Integer.parseInt(inst.substring(11,16),2);
+		Integer immediate = Integer.parseInt(inst.substring(16,32),2);
+		Integer result = 0;
+		Integer rsValue = s.readRegister(rs);
+		Integer immValue = 0;
+		
+		switch(op){
+			case "001000":
+				rsValue = binarySigned(completToLeft(Integer.toBinaryString(rsValue), '0', 32));
+				immValue = binarySigned(completToLeft(Integer.toBinaryString(immediate), '0', 32));
+				result = rsValue + immValue;
+				s.writeRegister(rt, result);
+
+				break;
+			case "001001":
+				result = rsValue + immediate;
+				s.writeRegister(rt, result);
+				break;
+		}
 		
 	}
 
@@ -59,6 +85,7 @@ public class MyMIPS implements MIPS{
 	}
 
 	private void tipoR(State s, String inst) {
+
 		Integer rs = Integer.parseInt(inst.substring(6,11),2);
 		Integer rt = Integer.parseInt(inst.substring(11,16),2);
 		Integer rd = Integer.parseInt(inst.substring(16,21),2);
@@ -67,7 +94,7 @@ public class MyMIPS implements MIPS{
 		Integer result = 0;
 		Integer rsValue = s.readRegister(rs);
 		Integer rtValue = s.readRegister(rt);
-		
+
 		switch(funct){
 			case "100000": //ADD
 				rsValue = binarySigned(completToLeft(Integer.toBinaryString(rsValue), '0', 32));
@@ -75,13 +102,31 @@ public class MyMIPS implements MIPS{
 				result = rsValue + rtValue;
 				s.writeRegister(rd, result);
 				break;
-			case "100001": //ADDU
+			case "100001": //ADDU TODO ANALISAR
 				result = rsValue + rtValue;
 				s.writeRegister(rd, result);
 				break;
+			case "100100": //AND
+				result = rsValue & rtValue;
+				s.writeRegister(rd, result);
+				break;
+			case "001000": //JR
+				s.setPC(rsValue);
+				break;
+			case "100111": //NOR
+				result = ~(rsValue | rtValue);
+				s.writeRegister(rd, result);	
+				break;
+			case "100101":
+				result = (rsValue | rtValue);
+				s.writeRegister(rd, result);
+				break;
+			//TODO AINDA FALTA
+				
 				
 		}
 		
+		//ADD
 		//ADDU - ADD Unsigned
 		//AND
 		//JR - Jump register
@@ -118,15 +163,20 @@ public class MyMIPS implements MIPS{
 	public static String invertDigits(String binaryInt) {
 		//complemento de 1
 	    String result = binaryInt;
-	    result = result.replace("0", " "); //temp replace 0s
-	    result = result.replace("1", "0"); //replace 1s with 0s
-	    result = result.replace(" ", "1"); //put the 1s back in
+	    result = result.replace("0", " "); 
+	    result = result.replace("1", "0"); 
+	    result = result.replace(" ", "1"); 
 	    return result;
 	}
 	
 	public static void main(String[] args) {
-		
-		System.out.println(binarySigned("00001010"));
+		try {
+			Simulator.setMIPS(new MyMIPS());
+			Simulator.setLogLevel(Simulator.LogLevel.INFO);
+			Simulator.start();
+		} catch (Exception e) {		
+			e.printStackTrace();
+		}	
 		
 	}
 
