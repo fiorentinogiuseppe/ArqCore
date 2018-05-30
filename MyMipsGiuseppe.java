aee70fc5b8ccceefa086da61907aa51181a7ce06
Giuseppe Fiorentino Neto <netogiuseppe@hotmail.com>
2018-05-30 09:17:20 -0300

    Update MyMipsGiuseppe.java

--- MyMipsGiuseppe.java
+++ MyMipsGiuseppe.java
@@ -63,23 +63,26 @@ public class MyMIPS implements MIPS{
 		Integer result = 0;
 		Integer rsValue = s.readRegister(rs);
 		
-		Integer SignExtImm;
-		Integer ZeroExtImm;
-		Integer BranchAddr;
-		String repetSignExtImm = inst.substring(15);
+		Integer SignExtImm; // {16{immediate[16]}, immediate} concatenar 16 immediate[16]'s e depois concatenar com immediate
+		Integer ZeroExtImm; // {16{1b'0}, immediate} concatenar 16 0's e depois concatenar com immediate
+		Integer BranchAddr; // {14{immediate[16]}, immediate , 2'b0} concatenar 14 immediate[16] e depois concatenar com immediate e depois com 2'b0
+		
+		//String de concatenacao 
+		String repetSignExtImm = inst.substring(16); 
 		String repetZeroExtImm = "0";
-		String repetBranchAddr = inst.substring(15);
+		String repetBranchAddr = inst.substring(16);
 		String zeros ="000";
 		
-		for(int i=0;i<15;i++){
-			repetSignExtImm += inst.substring(15);
+		//concatenacao
+		for(int i=0;i<15;i++){ //como ja ocorreu um elemento apenas. Agora precisa concatenas 15 valores
+			repetSignExtImm += inst.substring(16);
 			repetZeroExtImm += "0";
-			if(i<12) repetBranchAddr += inst.substring(15);
+			if(i<12) repetBranchAddr += inst.substring(15); //como so precisa de 14 e ja foi feito uma
 		}
-		//Integer repetValue= Integer.parseInt(repetSignExtImm);
-		SignExtImm = Integer.parseInt(inst.substring(16,32) + repetSignExtImm,2); //ao contrario
-		ZeroExtImm = Integer.parseInt(inst.substring(16,32) + repetZeroExtImm,2); //ao contrairo
-		BranchAddr = Integer.parseInt(zeros+inst.substring(16,32)+repetBranchAddr,2);
+
+		SignExtImm = Integer.parseInt((repetSignExtImm + inst.substring(16,32)),2); //valor do SignExtImm
+		ZeroExtImm = Integer.parseInt((repetZeroExtImm + inst.substring(16,32)),2); //valor do ZeroExtImm
+		BranchAddr = Integer.parseInt((repetBranchAddr + inst.substring(16,32) + zeros),2); //Valor do BranchAddr
 		
 		switch(op){
 			case "001000": //ADDI
@@ -113,15 +116,21 @@ public class MyMIPS implements MIPS{
 	}
 
 	private void tipoJ(State s, String inst, String op) {
-		Integer address = Integer.parseInt(inst.substring(6,32),2);
+		//Integer address = Integer.parseInt(inst.substring(6,32),2);
+		Integer JumpAddr = null;	 //TODO deselvolver essa equação
+		String part1=null; //PC+4op[31:28]
+		String part2=inst.substring(6,32); // address
+		String part3="000"; //2’b0
+		
+		JumpAddr = Integer.parseInt((part1+part2+part3),2);
 		
 		switch(op){
 			case "000010": //JUMP 
-				s.setPC(address);
+				s.setPC(JumpAddr);
 				break;
 			case "000011": //JUMP AND LINK
 				s.writeRegister(31,  s.getPC()+4); 
-				s.setPC(address);
+				s.setPC(JumpAddr);
 				break;				
 		}		
 	}
@@ -239,8 +248,43 @@ public class MyMIPS implements MIPS{
 }
 
 //TODO
-//the curly braces are for concatenation. The extra curly braces around 16{a[15]} are the replication operator.
+//the curly braces are for concatenation. The extra curly braces around 16{a[16]} are the replication operator.
 //{ 
-//a[15], a[15], a[15], a[15], a[15], a[15], a[15], a[15],
-//a[15], a[15], a[15], a[15], a[15], a[15], a[15], a[15]
-//}
+//a[16], a[15], a[15], a[15], a[15], a[15], a[15], a[15],
+//a[15], a[15], a[1
+/*
+assign result = {{16{a[15]}}, {a[15:0]}};
+is the same as:
+
+assign result[ 0] = a[ 0];
+assign result[ 1] = a[ 1];
+assign result[ 2] = a[ 2];
+assign result[ 3] = a[ 3];
+assign result[ 4] = a[ 4];
+assign result[ 5] = a[ 5];
+assign result[ 6] = a[ 6];
+assign result[ 7] = a[ 7];
+assign result[ 8] = a[ 8];
+assign result[ 9] = a[ 9];
+assign result[10] = a[10];
+assign result[11] = a[11];
+assign result[12] = a[12];
+assign result[13] = a[13];
+assign result[14] = a[14];
+assign result[15] = a[15];
+assign result[16] = a[15];
+assign result[17] = a[15];
+assign result[18] = a[15];
+assign result[19] = a[15];
+assign result[20] = a[15];
+assign result[21] = a[15];
+assign result[22] = a[15];
+assign result[23] = a[15];
+assign result[24] = a[15];
+assign result[25] = a[15];
+assign result[26] = a[15];
+assign result[27] = a[15];
+assign result[28] = a[15];
+assign result[29] = a[15];
+assign result[30] = a[15];
+assign result[31] = a[15];*/
